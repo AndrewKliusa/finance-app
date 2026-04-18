6d6c362cc2433c99fa7057d3c26e5f0d151ec8b5
AndrewKliusa <andriyklyusadev@gmail.com>
2026-04-18 08:52:19 -0700

    Added notification tokens.

--- schemas/env.schema.ts
+++ schemas/env.schema.ts
@@ -4,8 +4,9 @@ export const enviromentSchema = z.object({
     DATABASE_URL: z.url(),
     REDIS_URL: z.url(),
 
-    JWT_ACCESS_SECRET: z.string().min(32),
-    JWT_REFRESH_SECRET: z.string().min(32),
+    JWT_ACCESS_SECRET: z.string().min(64),
+    JWT_REFRESH_SECRET: z.string().min(64),
+    JWT_CONNECTION_SECRET: z.string().min(64),
 
     ADMIN_PASSWORD: z.string()
 })
