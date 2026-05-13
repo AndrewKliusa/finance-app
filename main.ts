e589ed8a16f29bc37421343197a3d933e09737d0
AndrewKliusa <andriyklyusadev@gmail.com>
2026-05-13 11:29:09 +0200

    Added some console logs to fix the failing deploy issue

--- main.ts
+++ main.ts
@@ -2,13 +2,16 @@ import 'dotenv/config'
 import { buildServer } from './server'
 import { enviromentSchema } from './schemas/env.schema'
 
+console.log("main.ts starting, NODE_ENV =", process.env.NODE_ENV, "PORT =", process.env.PORT)
 const env = enviromentSchema.parse(process.env)
 
 const server = buildServer()
-server.listen({ port: env.PORT, host: env.HOST }, (error) => {
+server.listen({ port: env.PORT, host: env.HOST }, (error, address) => {
     if (error) {
         server.log.error(error)
+        process.exit(1)
     }
+    server.log.info(`Server listening at ${address}`)
 })
 
 async function shutdown() {
